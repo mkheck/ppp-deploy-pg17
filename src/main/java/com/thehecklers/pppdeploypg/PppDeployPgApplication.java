76eaf9a542f0d5ce9874dfeec060e49d58aba72f
Mark Heckler <mark.heckler@gmail.com>
2024-01-19 10:50:52 -0600

    Initial commit

---
+++ src/main/java/com/thehecklers/pppdeploypg/PppDeployPgApplication.java
@@ -0,0 +1,13 @@
+package com.thehecklers.pppdeploypg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PppDeployPgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PppDeployPgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thehecklers/pppdeploypg/PppDeployPgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehecklers.pppdeploypg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PppDeployPgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
