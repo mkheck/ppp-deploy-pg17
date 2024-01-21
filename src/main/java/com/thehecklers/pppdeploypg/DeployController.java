7343edae0f448c000e1b8d705977b43804c7e7cb
Mark Heckler <mark.heckler@gmail.com>
2024-01-20 18:38:38 -0600

    Added controller

---
+++ src/main/java/com/thehecklers/pppdeploypg/DeployController.java
@@ -0,0 +1,14 @@
+package com.thehecklers.pppdeploypg;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DeployController {
+    @GetMapping
+    public String getDeploy() {
+        return "Deployed from GitHub!";
+    }
+}
